f1ba997fe1e582924945c238d8f35a193ee74e57
Bernardo Meireles <67381633+meirelesbc@users.noreply.github.com>
2022-02-15 20:56:10 +0000

    Hashes and menu

---
+++ hashspot.py
@@ -0,0 +1,91 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+hash_lenghts = {
+    4 : ["CRC16"],
+    8 : ["CRC32b",
+    "CRC32",
+    "ADLER32"],
+    16 : ["MD5-half",
+    "MySQL 3.2.3",],
+    32 : ["MD5",
+    "LanManager Hash",
+    "RIPEMD128",
+    "MD5 HMAC"],
+    40 : ["SHA1",
+    "HAVAL160",
+    "Tiger160",
+    "RIPEMD160"],
+    48 : ["Tiger192",
+    "HAVAL192"],
+    56 : ["SHA224",
+    "HAVAL224"],
+    64 : ["SHA256",
+    "GOST Hash",
+    "RIPEMD256",
+    "HAVAL256"],
+    80 : ["RIPEMD320"],
+    96 : ["SHA384"],
+    128 : ["Whirpool",
+    "SHA512"]
+}
+
+def check_hash(h):
+    l = len(h)
+
+    if h[:3] == "0x0":
+        if l == 54:
+            return "MSSQL 2005"
+        elif l == 94:
+            return "MSSQL 2000"
+
+    elif "$" in h:
+        if l == 36:
+            return "FreeBSD nthash"
+
+    elif "/" in h:
+        if l == 24:
+            return "CRYPT16"
+
+    elif (sum(1 for c in h if c.isupper())) == 1:
+        if l == 13:
+            return "DES Crypt"
+    
+    else:
+        return hash_lenghts[l]
+
+    return "Could not find any possible hashes"
+
+def menu():
+    print("""
+            ______              
+         .-'      `-.           
+       .'            `.         
+      /                \        
+     ;                 ;`       
+     |     HashSpot    |;       
+     ;                 ;|
+     '\               / ;       
+      \`.           .' /        
+       `.`-._____.-' .'         
+         / /`_____.-'           
+        / / /                   
+       / / /
+      / / /
+     / / /
+    / / /
+   / / /
+  / / /
+ / / /
+/ / /
+\/_/
+
+
+""")
+
+def method():
+    an = input("""Choose an option:
+
+[1] Check unique hash
+[2] Bulk checking
+[3] Exit""")
